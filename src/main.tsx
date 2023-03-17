4305e6aaa087aca3210abc089470b6d2fcf33951
DESKTOP-MRO5CVV\kaiti <kaitie.98@hotmail.com>
2023-03-17 18:19:33 +1000

    update routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Route, Routes } from "react-router";
+import { BrowserRouter } from "react-router-dom";
 import Blog from "./components/pages/Blog/Blog";
 import Home from "./components/pages/Home/Home";
 import "./index.scss";
 
 const BASE_PATH = "/KaitlynParsons";
 
-const router = createBrowserRouter(
-  [
+const router = [
     {
       path: "/",
       element: <Home />,
@@ -17,12 +17,16 @@ const router = createBrowserRouter(
       path: "/blog",
       element: <Blog />,
     },
-  ],
-  { basename: BASE_PATH }
-);
+  ];
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <BrowserRouter basename={BASE_PATH}>
+      <Routes>
+        {router.map((route) => (
+          <Route path={route.path} element={route.element} />
+        ))}
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
