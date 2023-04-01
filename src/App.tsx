abca2ca917f13d8ff9a9acfcc962af5dd3966977
DESKTOP-MRO5CVV\kaiti <kaitie.98@hotmail.com>
2023-04-01 13:36:44 +1000

    add gtag for prod

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
 import { Route, Routes } from "react-router-dom";
 import Navigation from "./components/layouts/Navigation/Navigation";
-import Blog from "./components/pages/Blog/Blog";
+// import Blog from "./components/pages/Blog/Blog";
+import ReactGA from "react-ga4";
 import About from "./components/pages/About/About";
+import { useEffect } from "react";
 
 const App = () => {
+    const isProd = import.meta.env.PROD;
+    
+    useEffect(() => {
+        if (isProd) {
+            ReactGA.initialize("G-SXHJ261CZ0");
+        }
+    });
+
     return (
         <Routes>
             <Route element={<Navigation />}>
