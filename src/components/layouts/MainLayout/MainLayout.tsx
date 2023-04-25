dec7579fda06157ebbe9ec1d58ec34c4e06b9ec3
DESKTOP-MRO5CVV\kaiti <kaitie.98@hotmail.com>
2023-04-25 16:11:37 +1000

    feat: simplify landing page

--- src/components/layouts/MainLayout/MainLayout.tsx
+++ src/components/layouts/MainLayout/MainLayout.tsx
@@ -1,11 +1,10 @@
-import { useEffect, useState } from "react";
-import { Outlet, useLocation, useNavigate } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import "./MainLayout.scss";
 import { IContact } from "../../../interfaces/IContact";
 import github from "../../../assets/github.svg";
 import linkedin from "../../../assets/linkedin.svg";
 import email from "../../../assets/email.svg";
-import Widget from "../Widget/Widget";
+import { yearsSinceFormatter } from "../../../helpers/DateHelper";
 
 const contact: IContact[] = [
   {
@@ -31,62 +30,17 @@ const contact: IContact[] = [
   }
 ];
 
-const navigationItems = [
-  {
-    title: 'Projects',
-    link: 'projects'
-  },
-  {
-    title: 'Blog',
-    link: 'blog'
-  },
-  {
-      title: 'About',
-      link: 'about'
-  }
-];
-
 const MainLayout = () => {
-  const navigating = useNavigate();
-  const location = useLocation();
-  const existingPath = navigationItems.find(item => item.link === location.pathname.slice(1));
-  
-  const [navigateState, setNavigateState] = useState(false);
-  const [selectedNav, setSelectedNav] = useState(existingPath || navigationItems[2]);
-
-  const navigate = (path: string) => {
-    const newRoute = navigationItems.find(nav => nav.link === path);
-    newRoute && setSelectedNav(newRoute);
-    navigating(path);
-    setNavigateState(true);
-  }
-
-  useEffect(() => {
-    if (navigateState) {
-      setNavigateState(false);
-    }
-
-  }, [navigateState]);
+  const timeCoding = `${yearsSinceFormatter('2018-04-24')} YEARS`;
 
   return (
     <div className="margin-lr">
-      <Widget />
       <header className="navHeading">
-        <h1 className="title-desktop">Kaitlyn Parsons</h1>
-        <h1 className="title-tablet">KP</h1>
-        <nav>
-          {navigationItems.map((item, index) => {
-            return (
-              <a
-                className={selectedNav.link === item.link ? "active" : ""}
-                key={index}
-                onClick={() => navigate(item.link)}
-              >
-                {item.title}
-              </a>
-            );
-          })}
-        </nav>
+        <div>
+          <h1 className="title-desktop">Kaitlyn Parsons</h1>
+          <h1 className="title-tablet">KP</h1>
+        </div>
+        <div>👩‍💻 {timeCoding}</div>
       </header>
       <section className="mainSection">
         <Outlet />
--- src/components/pages/About/Headline/Headline.tsx
+++ src/components/pages/About/Headline/Headline.tsx
@@ -5,12 +5,13 @@ import "./Headline.scss";
 const Headline = () => {
   return (
       <TabCard
+      title="About"
         content={
           <div>
             <h2 className="headline-title">
               <img src={icon} alt="icon" />
             </h2>
-            I'm a Software Engineer from Brisbane, Australia with a passion for technology and software development.
+            I'm a Software Engineer from Brisbane, Australia with a passion for technology and software development.<br />
             I focus on writing secure, clean, efficient and reusable code.<br /><br />
             Outside of software you'll find me watching tennis, at the gym, reading or eating yumcha &#129375;
           </div>
